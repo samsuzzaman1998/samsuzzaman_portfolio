cd33c415e6861070bd30fdd134f47de1401e25b0
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-06-13 09:09:06 +0600

    conncecting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h3 className="text-orange-700 text-center mt-10 uppercase">
+                hello react
+            </h3>
+        </>
+    );
 }
 
 export default App;
