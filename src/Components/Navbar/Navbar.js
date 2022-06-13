522a371e2ba6eaeced7ce32f4042f5d54ebf0197
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-06-14 00:12:28 +0600

    add navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -21,10 +21,75 @@ const Navbar = () => {
             setScrollValue(false);
         }
     };
+
+    const menuItems = (
+        <>
+            <li className="capitalize font-light text-neutral text-xl mb-3 lg:mb-0 lg:mr-4">
+                item1
+            </li>
+            <li className="capitalize font-light text-neutral text-xl mb-3 lg:mb-0 lg:mr-4">
+                item1
+            </li>
+            <li className="capitalize font-light text-neutral text-xl">
+                item1
+            </li>
+        </>
+    );
     return (
-        <nav className={`navbar-wrapper `} ref={navbarRef}>
+        <nav className={`navbar-wrapper z-50 `} ref={navbarRef}>
             <div id="navbar" className={`${scrollValue ? "sticky-nav" : ""}`}>
                 {/* navbar content goes here */}
+                <div className="navbar bg-primary text-base-100 lg:px-16 px-6">
+                    <div className="navbar-start">
+                        {/* <Link
+                    to="/"
+                    className="uppercase text-xl sm:text-4xl text-neutral font-semibold tracking-wider "
+                >
+                    kit-shop
+                </Link> */}
+                        <a
+                            href="/"
+                            className=" s_brand uppercase text-xl sm:text-4xl text-neutral font-semibold tracking-wider "
+                        >
+                            S
+                        </a>
+                    </div>
+                    <div className="navbar-end">
+                        <div className="dropdown dropdown-end block lg:hidden">
+                            <label
+                                tabIndex="0"
+                                className="btn btn-ghost lg:hidden"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h8m-8 6h16"
+                                    />
+                                </svg>
+                            </label>
+                            <ul
+                                tabIndex="0"
+                                className="menu menu-compact dropdown-content mt-5 p-2 shadow w-60 bg-primary"
+                            >
+                                {menuItems}
+                                {/* {userInfo} */}
+                            </ul>
+                        </div>
+                        {/* large menu */}
+                        <ul className="menu menu-horizontal p-0 hidden lg:flex ">
+                            {menuItems}
+                            {/* {userInfo} */}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     );
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,13 +3,33 @@ import Hero from "../Components/Hero/Hero";
 import Navbar from "../Components/Navbar/Navbar";
 
 const HomePage = () => {
+    const H1 = `
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                neque alias provident? Praesentium, nisi temporibus numquam
+                suscipit eligendi dignissimos ad, voluptatum omnis ex ipsa
+                consequuntur ut, facilis expedita corrupti reiciendis!
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                neque alias provident? Praesentium, nisi temporibus numquam
+                suscipit eligendi dignissimos ad, voluptatum omnis ex ipsa
+                consequuntur ut, facilis expedita corrupti reiciendis!
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                neque alias provident? Praesentium, nisi temporibus numquam
+                suscipit eligendi dignissimos ad, voluptatum omnis ex ipsa
+                consequuntur ut, facilis expedita corrupti reiciendis!
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+                neque alias provident? Praesentium, nisi temporibus numquam
+                suscipit eligendi dignissimos ad, voluptatum omnis ex ipsa
+                consequuntur ut, facilis expedita corrupti reiciendis!
+                
+    `;
     return (
         <>
             <Hero />
-            {/* <Navbar /> */}
+            <Navbar />
             {/* <div className="mx-auto content">
                 Bottom Content of Navbar
             </div> */}
+            {/* <h1 className="display_1">{H1}</h1> */}
         </>
     );
 };
